6a58c3c2ce0f040e5ceaf281546080c2c4a16e8a
Vinicius Zanfelici <viniciuszanfelici@hotmail.com>
2022-10-03 10:36:29 -0300

    listagem gasto finalizada

---
+++ app/src/main/java/br/edu/ifsp/duendindin_mobile/GastoListagemActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.duendindin_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GastoListagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem_gasto);
+    }
+}
