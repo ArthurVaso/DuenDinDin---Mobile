626ba8942602a1fb692342ac67caa3d19649b25e
israelfxs <israel.felix99@gmail.com>
2022-11-26 14:13:12 -0300

    corrigido forma que envia a data de nascimento do usuario para a api

--- app/src/main/java/br/edu/ifsp/duendindin_mobile/utils/URLAPI.java
+++ app/src/main/java/br/edu/ifsp/duendindin_mobile/utils/URLAPI.java
@@ -1,6 +1,8 @@
 package br.edu.ifsp.duendindin_mobile.utils;
 
 public class URLAPI {
+    //Atualizar URL baseada no IPv4 do IPCONFIG da máquina que estará rodando a API
+    //A maquina que roda a API deve estar na mesma rede que o ceular/emulador
     public String baseUrl = "http://192.168.0.106:3000/";
 
 }
