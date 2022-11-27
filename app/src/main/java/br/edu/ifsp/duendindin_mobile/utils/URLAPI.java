6126e0ff70f8bdb416e5cfcf1fa6b754e90fb7d6
ArthurVaso <arthur.v@aluno.ifsp.edu.br>
2022-11-27 12:19:44 -0300

    adicionando ip para a cloud

--- app/src/main/java/br/edu/ifsp/duendindin_mobile/utils/URLAPI.java
+++ app/src/main/java/br/edu/ifsp/duendindin_mobile/utils/URLAPI.java
@@ -5,4 +5,7 @@ public class URLAPI {
     //A maquina que roda a API deve estar na mesma rede que o ceular/emulador
     public String baseUrl = "http://192.168.0.106:3000/";
 
+    //Pode ser que este ip mude, se a cloud for encerrada e retomada
+    //public String baseUrl = "http://3.20.222.45:3000/";
+
 }
