656b715da4f56facdace59fea69770cc4472d766
ArthurVaso <arthur.v@aluno.ifsp.edu.br>
2022-12-04 15:45:22 -0300

    concertando o limite que estava errado

--- app/src/main/java/br/edu/ifsp/duendindin_mobile/utils/URLAPI.java
+++ app/src/main/java/br/edu/ifsp/duendindin_mobile/utils/URLAPI.java
@@ -1,9 +1,10 @@
 package br.edu.ifsp.duendindin_mobile.utils;
 
 public class URLAPI {
-    public String baseUrl = "http://192.168.0.106:3000/";
+    //Para testes local, trocar ip pelo do seu computador
+    //public String baseUrl = "http://192.168.0.106:3000/";
 
     //Pode ser que este ip mude, se a cloud for encerrada e retomada
-    //public String baseUrl = "http://3.20.222.45:3000/";
+    public String baseUrl = "http://3.20.222.45:3000/";
 
 }
